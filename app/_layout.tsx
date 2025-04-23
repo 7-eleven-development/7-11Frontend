9bc4a11b7558e09af897e66b94afee8eb6ea05c6
Alex Raunert <alexander.raunert@gmail.com>
2025-04-23 10:08:12 +0200

    added SafeAreaView to top level layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,21 @@
 import { Stack } from "expo-router";
+import { StyleSheet, Platform, StatusBar, SafeAreaView } from "react-native";
 
-export default function RootLayout() {
+const RootLayout = () => {
   return (
-    <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-    </Stack>
+    <SafeAreaView style={styles.safeArea}>
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      </Stack>
+    </SafeAreaView>
   );
-}
+};
+
+const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+  },
+});
+
+export default RootLayout;
