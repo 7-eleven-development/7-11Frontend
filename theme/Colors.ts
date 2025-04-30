f8aef683adf0c0df7361b46d337f081b4d15a730
Alex Raunert <alexander.raunert@gmail.com>
2025-04-30 11:27:19 +0200

    updated colors

--- theme/Colors.ts
+++ theme/Colors.ts
@@ -1,42 +1,38 @@
-export type Theme = "light" | "dark";
+const tintColorLight = "#0a7ea4";
+const tintColorDark = "#00ADB5";
 
 export const Colors = {
   light: {
-    text: " #11181C",
+    textColorDark: " #11181C",
+    textColorLigh: " #EEEEEE",
     background: " #D9D9D9",
-    tabBarBackground: "#393E46",
-    tint: "#0a7ea4",
-    icon: "rgb(87, 96, 103)",
+    tabBarBackground: "#D9D9D9",
+    tint: tintColorLight,
     tabIconDefault: "rgb(87, 96, 103)",
-    tabIconSelected: "rgb(43, 45, 46)",
+    tabIconSelected: tintColorLight,
 
-    primary: " #007AFF",
     secondary: " #5AC8FA",
     accent: " #FF9500",
     neutral: " #A9A9A9",
     error: " #FF3B30",
     success: " #34C759",
-
   },
 
   dark: {
-    text: " #11181C",
+    textColorDark: " #11181C",
+    textColorLigh: " #EEEEEE",
     background: " #222831",
     tabBarBackground: "#393E46",
-    tint: "#ffffff",
-    icon: "rgb(12, 158, 166)",
-    tabIconDefault: "rgb(27, 106, 171)",
-    tabIconSelected: " #ffffff",
+    tint: tintColorDark,
+    tabIconDefault: " #B0B0B0",
+    tabIconSelected: tintColorDark,
 
-    primary: " #0A84FF",
     secondary: " #64D2FF",
     accent: " #FFD60A",
-    neutral: " #B0B0B0",
+    neutral: " #567D7F",
     error: " #FF453A",
     success: " #32D74B",
-
   },
 };
-
 
 // lägga till CardBackground?
