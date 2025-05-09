f42aecd455b94dded4154173f5c1d2b08f69e97b
Alex Raunert <alexander.raunert@gmail.com>
2025-05-09 09:43:11 +0200

    prepared fetch func, uses generated mock data

---
+++ context/SoundLevel/fetchSoundLevel.ts
@@ -0,0 +1,20 @@
+import generateMockSoundLevelData from "./genereateMockData";
+
+export interface SoundLevelApiResponse {
+  message: string;
+  value: {
+    id: string;
+    soundLevel: number;
+  }[];
+}
+
+const fetchSoundLevel = async (): Promise<SoundLevelApiResponse> => {
+  try {
+    return generateMockSoundLevelData();
+  } catch (error) {
+    console.error("Error fetching sound level data:", error);
+    throw error;
+  }
+};
+
+export default fetchSoundLevel;
