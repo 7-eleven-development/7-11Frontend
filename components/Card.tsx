892a6548a43b5e0dc0b8f3e0341ee42619533994
Alex Raunert <alexander.raunert@gmail.com>
2025-05-14 09:39:29 +0200

    wip, updated cards

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,41 +1,129 @@
-import { View, StyleSheet } from "react-native";
-import { AntDesign } from "@expo/vector-icons";
-import  ThemedText  from "@/components/ThemedText";
+import { Pressable, StyleSheet, View } from "react-native";
+import { AntDesign, Entypo } from "@expo/vector-icons";
+import ThemedText from "@/components/ThemedText";
+import ThemedView from "./ThemedView";
+import { useRouter } from "expo-router";
+
+type CardStatus = "good" | "bad" | "normal";
+interface AirQualityData {
+  type: "airQuality";
+  data: {
+    pm1: number;
+    pm2: number;
+    pm10: number;
+    tvoc: number;
+  };
+  status: CardStatus;
+}
+
+interface SoundLevelData {
+  type: "soundLevel";
+  data: {
+    soundLevel: number;
+  };
+  status: CardStatus;
+}
+
+interface PulseData {
+  type: "pulse";
+  data: {
+    pulse: number;
+  };
+  status: CardStatus;
+}
+
+type CardData = AirQualityData | SoundLevelData | PulseData;
 
 type Props = {
-  title: string;
-  dataType: "soundLevel" | "gas" | "pulse";
-  data: number;
-  iconType?: "meh" | "smile" | "frown";
+  cardData: CardData;
 };
 
-const Card = ({ title, dataType, data, iconType }: Props) => {
-  const unit =
-    dataType === "soundLevel" ? "dB" : dataType === "gas" ? "ppm" : "bpm";
+const Card = ({ cardData }: Props) => {
+  const { type, data, status } = cardData;
+  const router = useRouter();
 
+  const Title =
+    type === "airQuality"
+      ? "Luftkvalitet"
+      : type === "soundLevel"
+      ? "Ljudnivå"
+      : "Puls";
   const icon =
-    iconType === "meh" ? (
-      <AntDesign name="meh" size={24} />
-    ) : iconType === "smile" ? (
-      <AntDesign name="smileo" size={24} />
-    ) : iconType === "frown" ? (
-      <AntDesign name="frowno" size={24} />
+    type === "airQuality" ? (
+      <Entypo name="air" size={24} color="black" />
+    ) : type === "soundLevel" ? (
+      <AntDesign name="sound" size={24} color="black" />
+    ) : type === "pulse" ? (
+      <AntDesign name="hearto" size={24} color="black" />
     ) : (
-      ""
+      <></>
+    );
+  const statusIcon =
+    status === "good" ? (
+      <AntDesign name="smileo" size={24} color="green" />
+    ) : status === "bad" ? (
+      <AntDesign name="frowno" size={24} color="red" />
+    ) : (
+      <AntDesign name="meh" size={24} color="orange" />
     );
 
+  const statusText =
+    type === "airQuality" ? (
+      <View
+        style={{ flex: 1, flexDirection: "column", alignItems: "flex-end" }}
+      >
+        <ThemedText>PM1: {data.pm1}</ThemedText>
+        <ThemedText>PM2: {data.pm2}</ThemedText>
+        <ThemedText>PM10: {data.pm10}</ThemedText>
+        <ThemedText>TVOC: {data.tvoc}</ThemedText>
+      </View>
+    ) : type === "soundLevel" ? (
+      <ThemedText type="subtitle">{data.soundLevel} dB</ThemedText>
+    ) : type === "pulse" ? (
+      <ThemedText type="subtitle">BPM: {data.pulse} </ThemedText>
+    ) : (
+      <ThemedText>Unknown Data</ThemedText>
+    );
+
+  const handleRoutePress = () => {
+    if (type === "airQuality") {
+      router.push("/(tabs)/air-quality");
+    } else if (type === "soundLevel") {
+      router.push("/(tabs)/sound-level");
+    } else if (type === "pulse") {
+      router.push("/(tabs)/pulse");
+    }
+  };
+
   return (
-    <View style={styles.card}>
-      <ThemedText>{title}</ThemedText>
-      <ThemedText>{data}</ThemedText>
-      <ThemedText>{unit}</ThemedText>
-      <ThemedText>{icon}</ThemedText>
-    </View>
+    <Pressable
+      style={({ pressed }) => [styles.pressable, pressed && { opacity: 0.9 }]}
+      onPress={() => {
+        handleRoutePress();
+      }}
+    >
+      <ThemedView style={styles.card}>
+        <View style={styles.contentContainer}>
+          {icon}
+          <ThemedText type="title">{Title}</ThemedText>
+        </View>
+        {statusIcon}
+        <View
+          style={{ flex: 1, flexDirection: "column", alignItems: "flex-end" }}
+        >
+          {statusText}
+        </View>
+      </ThemedView>
+    </Pressable>
   );
 };
 
 const styles = StyleSheet.create({
+  pressable: {
+    flex: 1,
+  },
   card: {
+    flex: 1,
     backgroundColor: "#f6f6f6",
     borderRadius: 8,
     padding: 16,
@@ -48,6 +136,11 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.25,
     shadowRadius: 3.84,
     elevation: 5,
+  },
+  contentContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 8,
   },
 });
 
