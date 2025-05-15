66b3a2a16eb0822d814b5ea4552d171ca09bc470
Alex Raunert <alexander.raunert@gmail.com>
2025-05-15 16:12:59 +0200

    installed react native map and using it in location screen

--- app/(tabs)/location.tsx
+++ app/(tabs)/location.tsx
@@ -1,9 +1,34 @@
+import { useEffect, useRef } from "react";
+import MapView, {
+  Callout,
+  Marker,
+  PROVIDER_GOOGLE,
+  Region,
+} from "react-native-maps";
+import { StyleSheet, View } from "react-native";
 import ThemedView from "@/components/ThemedView";
-import { Text } from "react-native";
+
 const Location = () => {
+  const INITIAL_REGION = {
+    latitude: 59.293314,
+    longitude: 18.0882497,
+    latitudeDelta: 2,
+    longitudeDelta: 2,
+  };
   return (
-    <ThemedView>
-      <Text>Location </Text>
+    <ThemedView
+      style={{
+        flex: 1,
+        padding: 10,
+      }}
+    >
+      <View style={{ flex: 1, marginBottom: 50, overflow: "hidden", borderRadius: 16 }}>
+        <MapView
+          style={StyleSheet.absoluteFillObject}
+          initialRegion={INITIAL_REGION}
+          provider={PROVIDER_GOOGLE}
+        ></MapView>
+      </View>
     </ThemedView>
   );
 };
